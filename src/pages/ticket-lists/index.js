a96223bcd78fa279438e174541e9a5184fb474fd
xuanhanhbn <xuanhanhbn2@gmail.com>
2023-07-27 00:26:51 +0700

    update project

--- src/pages/ticket-lists/index.js
+++
@@ -1,7 +0,0 @@
-import TicketPendings from 'src/views/ticket-pending'
-
-function TicketPending() {
-  return <TicketPendings />
-}
-
-export default TicketPending
--- src/pages/ticket-lists/ticket-details/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TicketDtails from 'src/views/ticket-pending/components/TicketDetails'
-
-function TicketDetail() {
-  return <TicketDtails />
-}
-
-export default TicketDetail
